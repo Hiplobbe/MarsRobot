6929962af045363d65dcafbd30eb3b1bc0becc6f
Hiplobbe <hiplobbe2@hotmail.com>
2023-03-30 11:05:35 +0200

    First commit
    First commit

---
+++ Mars Robot/Program.cs
@@ -0,0 +1,34 @@
+﻿using Mars_Robot;
+
+class Program
+{
+    private static long X { get; set; }
+    private static long Y { get; set; }
+
+    static void Main(string[] args)
+    {
+        string[] size = args[0].Split('x');
+        X = Convert.ToInt64(size[0]);
+        Y = Convert.ToInt64(size[1]);
+
+        Robot robot = new Robot();
+
+        char[] directions =
+            args[1].ToCharArray();
+
+        foreach (char dirr in directions)
+        {
+            if(dirr == 'F')
+            {
+                robot.MoveForward(X, Y);
+            }
+            else
+            {
+                robot.Turn(dirr);
+            }
+        }
+
+        Console.WriteLine(robot.X + " " + robot.Y + " " + robot.Direction.ToString());
+        Console.ReadLine();
+    }
+}
---
+++ Mars Robot/Robot.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars_Robot
+{
+     public class Robot
+    {
+        public Direction Direction { get; set; }
+        public long X { get; set; }
+        public long Y { get; set; }
+
+
+        public Robot()
+        {
+            Direction = Direction.North;
+            X = 1;
+            Y = 1;
+        }
+
+        public void MoveForward(long maxX, long maxY)
+        {
+            switch (this.Direction)
+            {
+                case Direction.North:
+                    if(Y == maxY)
+                    {
+                        return;
+                    }
+                    Y++;
+                    break;
+                case Direction.West:
+                    if (X == 1)
+                    {
+                        return;
+                    }
+                    X--;
+                    break;
+                case Direction.South:
+                    if (Y == 1)
+                    {
+                        return;
+                    }
+                    Y--;
+                    break;
+                case Direction.East:
+                    if (X == maxX)
+                    {
+                        return;
+                    }
+                    X++;
+                    break;
+            }
+        }
+
+        public void Turn(char direction)
+        {
+            switch (direction)
+            {
+                case 'L':
+                    if(Direction == Direction.North)
+                    {
+                        this.Direction = Direction.West;
+                        break;
+                    }
+
+                    Direction = Direction - 1;
+
+                    break;
+
+                case 'R':
+                    if (Direction == Direction.West)
+                    {
+                        Direction = Direction.North;
+                        break;
+                    }
+
+                    Direction = Direction + 1;
+
+                    break;
+            }
+        }
+    }
+
+    public enum Direction
+    {
+        North = 0,
+        East = 1,
+        South = 2,
+        West = 3
+    }
+}
